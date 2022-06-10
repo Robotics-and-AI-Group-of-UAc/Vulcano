eefb714406033648922812c23503cbabd20d4d0c
Francisco Pedro <66444231+franc-pedro@users.noreply.github.com>
2022-06-10 20:37:58 +0000

    Nova versão corrigida

---
+++ motores/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "ESP32MotorControl.h"
+
+// Inicialização dos motores
+ESP32MotorControl MotorControl = ESP32MotorControl();
+
+void setup() {
+  // 17,18,26,27 pinos dos motores no ESP32 
+  // correspondentes aos motores esquerdos e direitos
+  MotorControl.attachMotors(17, 18, 27, 26);
+}
+
+void loop() {  
+  MotorControl.motorForwardLeft(20);
+  MotorControl.motorReverseRight(20);
+  delay(5000);
+  MotorControl.motorReverseLeft(20);
+  MotorControl.motorForwardRight(20);
+  delay(5000); 
+  MotorControl.motorSTOP(0);
+  MotorControl.motorSTOP(1);
+  delay(5000);
+}
