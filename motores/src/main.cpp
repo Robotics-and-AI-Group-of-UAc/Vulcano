6ba6cf6e221ae9ab62cb16a6a91b31ef95d4d356
Francisco Pedro <66444231+franc-pedro@users.noreply.github.com>
2023-05-23 12:11:52 +0000

    Add files via upload

--- motores/src/main.cpp
+++ motores/src/main.cpp
@@ -1,46 +1,47 @@
-#include <Arduino.h>
 #include "ESP32MotorControl.h"
+#include <Arduino.h>
 
-// Motor Pins
-#define IN1 27
-#define IN2 26
-#define IN3 18
-#define IN4 17
-
-int velD = 30; // Velocidade motor direito
-int velE = 30; // velocidade motor esquerdo
-
-// MotorControl instance
+// Inicialização dos motores
 ESP32MotorControl MotorControl = ESP32MotorControl();
 
-void robotFwd() {
-    MotorControl.motorForward(0, velE);
-    MotorControl.motorForward(1, velD);
+/* Nas seguintes funções assumimos que:
+** Motor esquerdo - 0
+** Motor direito - 1
+*/
+void robot_forward(int speedLeft, int speedRight) {
+    MotorControl.motorForward(0, speedLeft);
+    MotorControl.motorForward(1, speedRight);
 }
-
-void robotRev() {
-    MotorControl.motorReverse(0, velE);
-    MotorControl.motorReverse(1, velD);
+void robot_reverse(int speedLeft, int speedRight) {
+    MotorControl.motorReverse(0, speedLeft);
+    MotorControl.motorReverse(1, speedRight);
 }
-
-void robotStop() {
-    MotorControl.motorsStop();
+void robot_turn_left(int speedLeft, int speedRight) {
+    MotorControl.motorForward(1, speedRight);
+    MotorControl.motorReverse(0, speedLeft);
+}
+void robot_turn_right(int speedLeft, int speedRight) {
+    MotorControl.motorForward(0, speedLeft);
+    MotorControl.motorReverse(1, speedRight);
+}
+void robot_stop() {
+    MotorControl.motorStop(0);
+    MotorControl.motorStop(1);
 }
 
 void setup() {
-    Serial.begin(115200);
-    
-    // Attach 2 motors
-    MotorControl.attachMotors(IN1, IN2, IN3, IN4, 1000);
+    // 17,18,26,27 pinos dos motores no ESP32
+    // correspondentes aos motores esquerdos e direitos
+    MotorControl.attachMotors(17, 18, 26, 27);
 }
 
 void loop() {
-    robotFwd();
-    delay(1000);
-    robotStop();
-    delay(1000);
-    robotRev();
-    delay(1000);
-    robotStop();
-    delay(1000);
+    robot_forward(50,50);
+    delay(5000);
+    robot_turn_left(50,50);
+    delay(5000);
+    robot_turn_right(50,50);
+    delay(5000);
+    robot_stop();
+    delay(2000);
 }
